175e8ec8d07e41e4307aaa1b5a69d4539739c9b6
nicolas <nicalpucci@gmail.com>
2021-10-06 13:45:14 +0200

    Localidad actual y sistema operativo

--- Prueba.java
+++ Prueba.java
@@ -2,6 +2,7 @@ public class Prueba {
 	public static void main(String args[]){
 	System.out.println("Nicolas Alfonso Pucci");
 	System.out.println("Caracas, 1999");
+	System.out.println("Trobajo del Camino, Windows 10 Home x64");
 
 	}
 }
